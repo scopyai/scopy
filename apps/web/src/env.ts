f869a95b4532065690e666c744a09823ded3ae09
Neat-Snap <Snapdragon1357@yandex.ru>
2026-05-30 19:21:15 +0300

    fixing auth

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,5 +1,8 @@
-import { z } from 'zod'
+import { z } from "zod"
 
-export const env = z.object({
-  VITE_API_BASE_URL: z.string().default('http://localhost:3001'),
-}).parse(import.meta.env)
+export const env = z
+  .object({
+    VITE_API_BASE_URL: z.string().default("http://localhost:3001"),
+    VITE_WEB_BASE_URL: z.string().default("http://localhost:3000"),
+  })
+  .parse(import.meta.env)
--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,6 @@
-import { env } from "@/env";
-import { createAuthClient } from "better-auth/react";
+import { env } from "@/env"
+import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: env.VITE_API_BASE_URL,
-});
+  baseURL: env.VITE_API_BASE_URL,
+})
